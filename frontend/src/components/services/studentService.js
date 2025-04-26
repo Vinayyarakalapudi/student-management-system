d8bb7a132276d914c1ae1e36deb31c6d68046621
Vinayyarakalapudi <vinayyarakalapudi2006@gmail.com>
2025-04-26 14:32:09 +0530

    Update studentService.js

--- frontend/src/components/services/studentService.js
+++ frontend/src/components/services/studentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const BASE_URL = `${import.meta.env.VITE_API_URL}/students`;
-const BASE_URL = 'https://student-management-system-4-7rum.onrender.com/students'; // backend deployed URL
+
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const getStudents = () => axios.get(BASE_URL);
 export const getStudentById = (id) => axios.get(`${BASE_URL}/${id}`);
