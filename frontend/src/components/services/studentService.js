6aa7a7abf04524f626479c3f56e11b2572d2df7f
Vinayyarakalapudi <vinayyarakalapudi2006@gmail.com>
2025-04-26 13:40:15 +0530

    Update studentService.js

--- frontend/src/components/services/studentService.js
+++ frontend/src/components/services/studentService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3001/students';
+// Update BASE_URL to the correct backend URL
+const BASE_URL = 'https://student-management-system-4-7rum.onrender.com/students'; // Your backend URL
 
 export const getStudents = () => axios.get(BASE_URL);
 export const getStudentById = (id) => axios.get(`${BASE_URL}/${id}`);
