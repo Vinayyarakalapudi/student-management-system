802c25e73f292537d09352e961d6a5de44a303dc
Vinayyarakalapudi <vinayyarakalapudi2006@gmail.com>
2025-04-26 14:40:12 +0530

    Update studentService.js

--- frontend/src/components/services/studentService.js
+++ frontend/src/components/services/studentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = 'https://student-management-system-4-7rum.onrender.com/students';
 
 export const getStudents = () => axios.get(BASE_URL);
 export const getStudentById = (id) => axios.get(`${BASE_URL}/${id}`);
